476d527ed3262d10c9f87d8871d390f8b80b467a
adityeahB <1adityaneil@gmail.com>
2026-04-02 14:40:51 +0100

    Add pdfmake to optimizeDeps and build options

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  optimizeDeps: {
+    include: ['pdfmake']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/pdfmake/, /node_modules/]
+    }
+  }
 })
